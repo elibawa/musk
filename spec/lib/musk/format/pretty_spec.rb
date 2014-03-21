cb9d018d9b6b15a44b518983e46f37b9ab9f48c2
Eugene Pempel <eugene.pempel@gmail.com>
2014-03-21 20:45:15 +0400

    add tests for Musk::Format::Pretty

---
+++ spec/lib/musk/format/pretty_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Musk::Format::Pretty do
+  describe ".format(tracks)" do
+    let(:tracks) do
+      [build(:jets_track), build(:kamakura_track)]
+    end
+
+    let(:stdout) do
+      tracks.map do |track|
+        "Path:     #{track.path}\n"\
+        "Title:    #{track.title}\n"\
+        "Position: #{track.position}\n"\
+        "Artist:   #{track.artist}\n"\
+        "Album:    #{track.album}\n"\
+        "Genre:    #{track.genre}\n"\
+        "Year:     #{track.year}\n"\
+        "Comment:  #{track.comment}\n"\
+      end.join("\n")
+    end
+
+    it "should print tracks to STDOUT in the pretty format" do
+      capture(:stdout) { Musk::Format::Pretty.format(tracks) }.should eq(stdout)
+    end
+  end
+end
