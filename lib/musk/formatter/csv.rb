36a7d08ef672b9e2bb86d45bb68d1fb115326490
Eugene Pempel <eugene.pempel@gmail.com>
2014-03-28 01:05:29 +0400

    refactor Musk::Formatter::Pretty

--- lib/musk/formatter/csv.rb
+++ lib/musk/formatter/csv.rb
@@ -6,12 +6,18 @@ module Musk
       end
 
       def initialize(tracks)
-        @tracks = tracks.map {|t| Musk::Decorator::PrintableTrack.new(t)}
+        @tracks = tracks.map { |t| Musk::Decorator::PrintableTrack.new(t) }
         @fields = [:path, :title, :position, :artist, :release, :genre, :year, :comment]
       end
 
       def print
-        @tracks.each {|t| puts @fields.map {|f| t.send(f)}.join(",")}
+        @tracks.each { |t| puts printed(t) }
+      end
+
+      private
+
+      def printed(track)
+        @fields.map { |f| track.send(f) }.join(",")
       end
     end
   end
